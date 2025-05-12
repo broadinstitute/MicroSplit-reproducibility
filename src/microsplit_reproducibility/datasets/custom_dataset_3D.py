24c15935206005a5b21e34cf549fa04c1aec3c4b
federico-carrara <federico1.carrara@mail.polimi.it>
2025-05-12 17:54:46 +0200

    refac: added custom dataset for 2D and 3D customn data cases

--- src/microsplit_reproducibility/datasets/custom_dataset_3D.py
+++ src/microsplit_reproducibility/datasets/custom_dataset_3D.py
@@ -11,23 +11,7 @@ from careamics.lvae_training.dataset.utils.data_utils import get_datasplit_tuple
 from careamics.dataset.dataset_utils.dataset_utils import reshape_array
 
 
-def _load_one_file_2D(fpath):
-    """Load a single 2D image file."""
-    data = tifffile.imread(fpath)
-    if len(data.shape) == 2:
-        axes = 'YX'
-    elif len(data.shape) == 3:
-        axes = 'SYX' 
-    elif len(data.shape) == 4:
-        axes = 'STYX'
-    else: 
-        raise ValueError(f"Invalid data shape: {data.shape}")
-    data = reshape_array(data, axes)
-    data = data.reshape(-1, data.shape[-2], data.shape[-1])
-    return data
-
-
-def _load_one_file_3D(fpath):
+def load_one_file(fpath):
     """Load a single 3D image file."""
     data = tifffile.imread(fpath)
     if len(data.shape) == 3:
@@ -43,17 +27,7 @@ def _load_one_file_3D(fpath):
     return data
 
 
-def load_one_file(fpath, ndim: Literal[2, 3]):
-    """Load a single image file."""
-    if ndim == 2:
-        return _load_one_file_2D(fpath)
-    elif ndim == 3:
-        return _load_one_file_3D(fpath)
-    else:
-        raise ValueError(f"Invalid dimension: {ndim}")    
-
-
-def load_data(datadir, ndim: Literal[2, 3]):
+def load_data(datadir):
     data_path = Path(datadir)
 
     channel_dirs = sorted(p for p in data_path.iterdir() if p.is_dir())
@@ -61,7 +35,7 @@ def load_data(datadir, ndim: Literal[2, 3]):
 
     for channel_dir in channel_dirs:
         image_files = sorted(f for f in channel_dir.iterdir() if f.is_file())
-        channel_images = [load_one_file(image_path, ndim) for image_path in image_files]
+        channel_images = [load_one_file(image_path) for image_path in image_files]
             
         channel_stack = np.concatenate(channel_images, axis=0) # FIXME: this line works iff images have
         # a singleton channel dimension. Specify in the notebook or change with `torch.stack`??
@@ -79,7 +53,7 @@ def get_train_val_data(
     test_fraction=None,
     **kwargs,
 ):
-    data = load_data(datadir, len(data_config.image_size))
+    data = load_data(datadir)
     train_idx, val_idx, test_idx = get_datasplit_tuples(
         val_fraction, test_fraction, len(data)
     )
